cda26a70621d1413d305b285add1b3c43713d17f
Enrique Rocha Benatti <rochabenattienrique@gmail.com>
2024-01-22 00:04:23 -0300

    stopped on resize error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
+//static
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+//routes
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 2,
+      title: 'My 2nd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 3,
+      title: 'My 3rd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    }
+  ]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState('');
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {};
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter((post) => post.id !== id);
+    setPosts(postsList);
+    navigate('/');
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <Header title='Reactjs Blog' />
+      <Nav search={search} setSearch={setSearch} />
+      <Routes>
+        <Route path='/' element={<Home posts={posts} />} />
+        <Route
+          path='/post'
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route path='/post/:id' element={<PostPage posts={posts} handleDelete={handleDelete} />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
